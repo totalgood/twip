a4cba0242ce21297ac21f4cebff0d82fd8429dd2
Hobson Lane <hobson@talentpair.com>
2015-12-09 11:22:21 -0800

    good luck with this you hacker-scrapers

---
+++ twip/settings_secret.py
@@ -0,0 +1,14 @@
+# Don't commit this file to any public repository
+# Fill in these constants with your actual keys and
+# Keep it safely confined to your local laptop by ensuring it is `.gitignore`d
+
+# KEY and SECRET are required to download new tweets
+TWITTER_API_KEY = '25AlphaNumericCharactersX'
+TWITTER_API_SECRET = '50AlphaNumericCharactersX50AlphaNumericCharactersX'
+
+# Optional account identifying information that doesn't need to be protected
+TWITTER_API_OWNER = 'hobsonlane'  # the app owner's @username
+TWITTER_API_OWNER_ID = 59275999   # see the app
+
+# This is how to use these keys to instantiate a Twython connection to the twitter API
+# twyth = Twython(TWITTER_API_KEY, TWITTER_API_SECRET)

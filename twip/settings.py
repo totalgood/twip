67035f4a7a941b8da1a0d69ae2a1d8198e5d2f27
Hobson Lane <github@totalgood.com>
2016-04-22 23:09:59 -0700

    better settings_secret instructions

--- twip/settings.py
+++ twip/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-# Secret keys and passwords
+# Twitter API Secrets
 from twip.settings_secret import TWITTER_API_KEY, TWITTER_API_SECRET
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
--- twip/settings_template.py
+++ twip/settings_template.py
@@ -1,10 +1,18 @@
-# Don't commit this file to any public repository
-# Fill in these constants with your actual keys and
-# Keep it safely confined to your local laptop by ensuring it is `.gitignore`d
+# 1. copy this file to settings_secret.py
+# 2. make sure the settings_secret.py file is ignored (should be listed in .gitignore)
+# 3. EITHER...
+#   a) add your TWITTER secrets directly to this file in the `except:` block below
+#    **OR**
+#   b) set your environment variables to contain your keys (see below for env var names)
+# 4. `twyth = Twython(settings_secret.TWITTER_API_KEY, settings_secret.TWITTER_API_SECRET)`
 
-# KEY and SECRET are required to download new tweets
-TWITTER_API_KEY = '25AlphaNumericCharactersX'
-TWITTER_API_SECRET = '50AlphaNumericCharactersX50AlphaNumericCharactersX'
+# TWITTER_API_KEY and _SECRET are required to download tweets
+try:
+    TWITTER_API_KEY = os.getenv('TWITTER_API_KEY')
+    TWITTER_API_SECRET = os.getenv('TWITTER_API_SECRET')
+except:
+    TWITTER_API_KEY = '25AlphaNumericCharactersX'
+    TWITTER_API_SECRET = '50AlphaNumericCharactersX50AlphaNumericCharactersX'
 
 # Optional account identifying information that doesn't need to be protected
 TWITTER_API_OWNER = 'hobsonlane'  # the app owner's @username

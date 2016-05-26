dc86e8c0886ca9a8b64184cc1b26be2ea70d5306
Hobson Lane <github@totalgood.com>
2016-05-26 07:41:29 -0700

    cat_tweets now has --numtweets or -n arg

--- twip/scripts/cat_tweets.py
+++ twip/scripts/cat_tweets.py
@@ -51,34 +51,46 @@ def parse_args(args):
     parser.add_argument(
         '-v',
         '--verbose',
+        help='Whether to show progress messages on stdout',
         action='store_true')
     parser.add_argument(
         '--version',
+        help='print twip package version and exit.',
         action='version',
         version='twip {ver}'.format(ver=__version__))
     parser.add_argument(
         '-p',
         '--path',
+        help='path to data dir (json input and csv output files)',
         default=DATA_PATH)
     parser.add_argument(
         '-t',
         '--tweetfile',
+        help='file to store normalized table (csv) of tweets and features (metadata)',
         default='all_tweets.csv')
+    parser.add_argument(
+        '-n',
+        '--numtweets',
+        help='maximum number of tweets to process',
+        type=int,
+        default=1000000)
     parser.add_argument(
         '-g',
         '--geofile',
+        help="file to store tweet lat/lon information (few use it)",
         default='geo_tweets.csv')
     return parser.parse_args(args)
 
 
 def main(args):
+    """API with args object containing configuration parameters"""
     global logging, log
 
     args = parse_args(args)
     logging.basicConfig(format=LOG_FORMAT,
                         level=logging.DEBUG if args.verbose else logging.INFO,
                         stream=sys.stdout)
-    df = cat_tweets(path=args.path, verbosity=args.verbose + 1)
+    df = cat_tweets(path=args.path, verbosity=args.verbose + 1, numtweets=args.numtweets)
     log.info('Combined {} tweets'.format(len(df)))
     df = drop_nan_columns(df)
     save_tweets(df, path=args.path, filename=args.tweetfile)
@@ -89,10 +101,11 @@ def main(args):
 
 
 def run():
+    """Entry point for command line script"""
     main(sys.argv[1:])
 
 
-def cat_tweets(filename='all_tweets.json', path=DATA_PATH, verbosity=1):
+def cat_tweets(filename='all_tweets.json', path=DATA_PATH, verbosity=1, numtweets=10000000):
     """Find json files that were dumped by tweetget and combine them into a single CSV
 
     Normalize some (lat/lon)"""
@@ -131,8 +144,12 @@ def cat_tweets(filename='all_tweets.json', path=DATA_PATH, verbosity=1):
             df['lat'] = np.nan * np.ones(len(df))
             df['lon'] = np.nan * np.ones(len(df))
         df_all = df_all.append(df)
+        # this would be a good time to incrementally save these rows to disc
         del df
         loaded_size += meta['size']
+        if len(df_all) >= numtweets:
+            # FIXME use average rate of 400 tweets/MB to calculate better progressbar size at start
+            break
         if pbar:
             pbar.update(loaded_size / 1e6)
     if pbar:
@@ -161,24 +178,32 @@ def drop_columns(df, columns=u'common_columns.json'):
 
 
 def get_geo(df, path=DATA_PATH, filename=u'geo_tweets.csv'):
-    # path = path.encode()
-    geo = df[~df.lat.isnull() & ~df.lon.isnull()].copy()
+    path = path.encode()
+    try:
+        geo = df[~df.lat.isnull() & ~df.lon.isnull()].copy()
+    except AttributeError:
+        geo = pd.DataFrame(columns=df.columns)
     if isinstance(filename, base.str):
-        geo.to_csv(os.path.join(path, filename), encoding='utf8',  # compression='gzip',
+        geo.to_csv(os.path.join(path, filename), encoding='utf8',
                    escapechar=None, quotechar='"', quoting=pd.io.common.csv.QUOTE_NONNUMERIC)
     return geo
 
 
 def save_tweets(df, path=DATA_PATH, filename=u'all_tweets.csv'):
-    #path = path.encode
+    path = path.encode()
     filename = os.path.join(path, filename)
-    df_size = len(df) * 2402.9691 / 1e6
+    # your first "model": predict the size of a file based on the number of rows of tweet data (average rate = linear regression)
+    df_size = len(df) * 2403 / 1e6
 
     T0 = time.time()
-    log.info('Saving tweets in {} which should take around {:.1f} MB and {:.1f} min (utf-8 encoding in Pandas .to_csv is VERY slow)...'.format(
-             filename, df_size, 2.0 * df_size / 60.))
-    # additional to_csv options to consider: compression='gzip', escapechar=None, 
-    df.to_csv(os.path.join(path, filename), encoding='utf8', quotechar='"', quoting=pd.io.common.csv.QUOTE_NONNUMERIC)
+    log.info('Saving {} tweets in {} which should take around {:.1f} MB and {:.1f} min (utf-8 encoding in Pandas .to_csv is VERY slow)...'.format(
+             len(df), filename, df_size, 2.0 * df_size / 60.))
+    # consider using to_csv(compression='gzip', escapechar=None) 
+    try:
+        df.to_csv(os.path.join(path, filename), encoding='utf8', quotechar='"', quoting=pd.io.common.csv.QUOTE_NONNUMERIC,
+                               compression='gzip')
+    except ValueError:  # encoding + compression not yet supported in Python 2
+        df.to_csv(os.path.join(path, filename), encoding='utf8', quotechar='"', quoting=pd.io.common.csv.QUOTE_NONNUMERIC)
     T1 = time.time()
     log.info('Saved {} tweets in {:.1f} min'.format(len(df), (T1 - T0) / 60.))
 
